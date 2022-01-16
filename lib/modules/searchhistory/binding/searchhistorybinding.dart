8c943a1a590a75466c84715953cfa8b49ad1df7f
sangeethasakthivel99 <sakthivelsangeetha99@gmail.com>
2022-01-16 22:52:00 +0530

    Remove unnecessary Dependencies

--- lib/modules/searchhistory/binding/searchhistorybinding.dart
+++ lib/modules/searchhistory/binding/searchhistorybinding.dart
@@ -1,13 +1,9 @@
 import 'package:get/get.dart';
 import 'package:pocket_dictionary/modules/searchhistory/controller/searchhistorycontroller.dart';
-import 'package:pocket_dictionary/modules/searchresult/controller/searchresultcontroller.dart';
-import 'package:pocket_dictionary/modules/searchresult/repo/searchrepo.dart';
 
 class SearchHistoryBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<SearchRepo>(() => SearchRepo());
-    Get.lazyPut<SearchResultController>(() => SearchResultController());
     Get.lazyPut<SearchHistoryController>(() => SearchHistoryController());
   }
 
